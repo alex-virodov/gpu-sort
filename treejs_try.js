be07964b9e763123f712cfd6bd89ee12c5ef62cd
Alexandr Virodov <alexandr.vir@gmail.com>
2024-03-05 16:02:48 -0500

    position & color working with custom shader. Only for the model data (all floats, no uchar support for input due to three.js limitations).

--- treejs_try.js
+++ treejs_try.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // const [canvasWidth, canvasHeight] = [window.innerWidth, window.innerHeight]
-const [canvasWidth, canvasHeight] = [240, 120]
+const [canvasWidth, canvasHeight] = [400, 400]
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(/*fov=*/75, canvasWidth / canvasHeight, 0.1, 1000 );
 
@@ -22,10 +22,13 @@ const size = 10; const divisions = 10;
 const gridHelper = new THREE.GridHelper( size, divisions ); 
 scene.add( gridHelper );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Add box.
+{
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+}
 
 // https://stackoverflow.com/questions/29610102/orbitcontrols-how-to-load-last-view-position-and-rotation
 function loadControls(controls) {
@@ -71,6 +74,24 @@ function checkLineStartsWith(index, expected, actual) {
     }
 }
 
+function countPlyFloatsUchars(properties) {
+    // For now, assuming properties are a number of floats followed by a number of uchars, which is
+    // true for the data I have.
+    let numFloatsCounter = 0;
+    while (numFloatsCounter < properties.length && 
+        properties[numFloatsCounter].type == 'float') { numFloatsCounter++; }
+    let numUcharsCounter = numFloatsCounter;
+    while (numUcharsCounter < properties.length && 
+        properties[numUcharsCounter].type == 'uchar') { numUcharsCounter++; }
+    console.log('numFloatsCounter=', numFloatsCounter, 'numUcharsCounter=', numUcharsCounter)
+    
+    const numFloats = numFloatsCounter;
+    const numUchars = numUcharsCounter - numFloatsCounter; // Note: the uchars counter includes floats.
+    console.log('numFloats=', numFloats, 'numUchars=', numUchars);
+    
+    return {numFloats: numFloats, numUchars: numUchars};
+}
+
 async function readPly(url) {
     console.log('readPly url=' + url);
     const response = await fetch(url);
@@ -83,7 +104,7 @@ async function readPly(url) {
     // console.log('buffer=', buffer);
 
     // TODO: more robust header decoding, allow arbitrary len.
-    const headerView = new Int8Array(buffer, 0, 1024);
+    const headerView = new Int8Array(buffer, 0, 2048);
     // console.log('headerView=', headerView);
 
     var headerString = new TextDecoder().decode(headerView);
@@ -120,21 +141,15 @@ async function readPly(url) {
     const dataBuffer = buffer.slice(startBinaryLocation);
 
     /* No need to parse vertices, we'll do that with OpenGL attribute bindings (I hope :)
+    // haha, no, not supported in three.js yet, and may not be feasible becase underlying webgl
+    // doesn't do it efficiently anyway ("We recently found that using interleaved buffers with 
+    // attributes of different types leads to the creation of multiple buffers in WebGL, which 
+    // duplicates data on the GPU and defeats the purpose of interleaving data in the first place"):
+    // https://github.com/mrdoob/three.js/pull/21656
+    // And in any case, the trained models are all floats, only the 'input.ply' has uchars.
 
     // Structured binary reading: https://github.com/gkjohnson/js-struct-data-view
-    // For now, assuming properties are a number of floats followed by a number of uchars, which is
-    // true for the data I have.
-    let numFloatsCounter = 0;
-    while (numFloatsCounter < properties.length && 
-        properties[numFloatsCounter].type == 'float') { numFloatsCounter++; }
-    let numUcharsCounter = numFloatsCounter;
-    while (numUcharsCounter < properties.length && 
-        properties[numUcharsCounter].type == 'uchar') { numUcharsCounter++; }
-    console.log('numFloatsCounter=', numFloatsCounter, 'numUcharsCounter=', numUcharsCounter)
-    
-    const numFloats = numFloatsCounter;
-    const numUchars = numUcharsCounter - numFloatsCounter; // Note: the uchars counter includes floats.
-    console.log('numFloats=', numFloats, 'numUchars=', numUchars);
+    // todo: call countPlyFloatsUchars(properties), if needed.
 
     const vertexSizeInBytes = numFloats * 4 + numUchars;
     console.log(`vertexSizeInBytes=${vertexSizeInBytes}, numVertices=${numVertices}, dataBuffer.byteLength=${dataBuffer.byteLength}`);
@@ -156,5 +171,45 @@ function animate() {
 }
 animate();
 
-const data = await readPly('data/models/train/input.ply');
+const data = await readPly('data/models/train/point_cloud/iteration_7000/point_cloud.ply');
 console.log('readPly result=', data);
+
+// Point cloud example from https://threejs.org/docs/index.html#api/en/materials/PointsMaterial
+{
+    const dataCounts = countPlyFloatsUchars(data.properties);
+    const interleavedBuffer = new THREE.InterleavedBuffer(new Float32Array(data.dataBuffer), /*stride=*/dataCounts.numFloats);
+    console.log('interleavedBuffer=', interleavedBuffer);
+    const ibattrXyz = new THREE.InterleavedBufferAttribute(interleavedBuffer, 3, 0);
+    const ibattrRgb = new THREE.InterleavedBufferAttribute(interleavedBuffer, 3, 6);
+    console.log('ibattrXyz=', ibattrXyz);
+
+
+
+    const material = new THREE.ShaderMaterial( {
+
+        uniforms: {
+        },
+        vertexShader: document.getElementById( 'vertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+
+    } );
+
+
+    /*const vertices = [];
+
+    for ( let i = 0; i < 1000; i ++ ) {
+        const x = THREE.MathUtils.randFloatSpread( 20 );
+        const y = THREE.MathUtils.randFloatSpread( 20 );
+        const z = THREE.MathUtils.randFloatSpread( 20 );
+
+        vertices.push( x, y, z );
+    }*/
+
+    const geometry = new THREE.BufferGeometry();
+    // geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+    geometry.setAttribute( 'position', ibattrXyz);
+    geometry.setAttribute( 'color', ibattrRgb);
+    // const material = new THREE.PointsMaterial( { color: 0x888888, size: 0.1 } );
+    const points = new THREE.Points( geometry, material );
+    scene.add( points );
+}
